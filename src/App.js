2b5edcf021ca393ee2e2986c25371392e506385b
Maksym Diachenko <max97292@gmail.com>
2021-12-03 09:15:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+// to calculate typing speed
+// words typed / minutes
+// words typed = (characters - typos) / 5
+
+const secondsToCount = 10;
+const paragraph = `Coding is the best. We are able to build something from scratch. It is literally imagination incarnate. Solving our own problems through coding is one of the coolest things we could do!`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* sidebar */}
+      <div className="sidebar">
+        <div className="timer">00</div>
+        <button className="start">Start</button>
+        <button className="reset">Reset</button>
+      </div>
+
+      <div className="content">
+        {/* show the paragraph */}
+        <p>{paragraph}</p>
+
+        {/* show the textarea */}
+        <form>
+          <textarea rows="10" placeholder="Test your typing skills..." />
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
